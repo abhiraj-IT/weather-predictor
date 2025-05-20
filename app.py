49959a0be304ef6f6418772da4f0570a7ce4e6b3
Abhishek kumar Singh. <123228958+abhiraj-IT@users.noreply.github.com>
2025-05-20 15:29:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if st.button("🔍 Predict Weather"):
         api_url = "https://bamsrad.us-east-2.aws.modelbit.com/v1/weather_api/latest"
 
         headers = {
-            "Authorization": f"Bearer {st.secrets['miK4/iqEcH:msM9TmRFMKZG0aFJHEMQKyWvzT25HSzLl6LwLVXKf2Y0o=']}",
+            "Authorization": f"Bearer {st.secrets['miEZHaNjj+:msJeGibAaWlnjIB3rp2K8XxdgYGmPG8AI7PSQwBFkijJ8=']}",
             "Content-Type": "application/json"
         }
 
